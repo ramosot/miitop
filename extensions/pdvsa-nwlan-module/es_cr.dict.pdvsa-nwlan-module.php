7c6add0eeb38b0b69570653502ae6872bdf157f4
Oswald Ramos <ramosot@pdvsa.com>
2019-03-25 15:36:31 -0400

    SW,RT,AP,WLANC

---
+++ extensions/pdvsa-nwlan-module/es_cr.dict.pdvsa-nwlan-module.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// Class: Switche
+//
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:Switche' => 'Switche',
+	'Class:Switche+' => 'Switche',
+	'Class:Switche/Attribute:switch_type' => 'Tipo Switche',
+	'Class:Switche/Attribute:switch_type+' => 'Tipo Switche',
+	'Class:Switche/Attribute:switch_type/Value:access' => 'Acceso',
+	'Class:Switche/Attribute:switch_type/Value:access+' => 'Acceso',
+	'Class:Switche/Attribute:switch_type/Value:core' => 'Core / Distribución',
+	'Class:Switche/Attribute:switch_type/Value:core+' => 'Core / Distribución',
+	
+));
+
+//
+// Class: Router
+//
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:Router' => 'Router',
+	'Class:Router' => 'Router',
+	'Class:Router/Attribute:router_type' => 'Tipo Router',
+	'Class:Router/Attribute:router_type+' => 'Tipo Router',
+	'Class:Router/Attribute:router_type/Value:voicegateway' => 'Gateway de Voz',
+	'Class:Router/Attribute:router_type/Value:voicegateway+' => 'Gateway de Voz',
+	'Class:Router/Attribute:router_type/Value:routerwan' => 'Router WAN',
+	'Class:Router/Attribute:router_type/Value:routerwan+' => 'Router WAN',
+	
+));
+
+//
+// Class: AccessPoint
+//
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:AccessPoint' => 'Punto de Acceso',
+	'Class:AccessPoint' => 'Punto de Acceso',
+	'Class:AccessPoint/Attribute:apoint_type' => 'Tipo',
+	'Class:AccessPoint/Attribute:apoint_type+' => 'Tipo',
+	'Class:AccessPoint/Attribute:apoint_type/Value:autonome' => 'Autonomo',
+	'Class:AccessPoint/Attribute:apoint_type/Value:autonome+' => 'Autonomo',
+	'Class:AccessPoint/Attribute:apoint_type/Value:ligthweight' => 'Lightweight',
+	'Class:AccessPoint/Attribute:apoint_type/Value:ligthweight' => 'Lightweight',
+	
+));
+
+//
+// Class: WLANController
+//
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:WLANController' => 'Controlador WLAN',
+	'Class:WLANController' => 'Controlador WLAN',
+	'Class:WLANController/Attribute:accesspoint_list' => 'Puntos de Acceso',
+	'Class:WLANController/Attribute:accesspoint_list+' => 'Puntos de Acceso',
+	
+));
+
+?>
